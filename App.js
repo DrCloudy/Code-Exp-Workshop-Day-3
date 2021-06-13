e4cec7a2e168add4b60272fefb1381680eb34004
DrCloudy <rmriaz444@gmail.com>
2021-06-14 02:25:24 +0800

    added header styling

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+function notesScreen() {
+  return <View style={styles.container}></View>
+}
+
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Notes" component={notesScreen} options = {{
+          headerTitle: "Notes",
+          headerStyle: styles.headerStyle,
+          headerTitleStyle: styles.headerTitleStyle,
+        }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eee',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  headerStyle: {
+    height: 80,
+    backgroundColor: 'pink',
+  },
+  headerTitleStyle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    paddingBottom: 30,
+  },
 });
+ 
